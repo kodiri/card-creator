47363503c7fbef800dd6b7b977693e57f923d607
katherinf <katherinfons@gmail.com>
2019-09-08 11:19:20 +0100

    Cards, Icons and fonts packages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Canva from './Canva/Canva.js';
 import Navbar from './Navbar/Navbar.js';
 import Tools from './Tools/Tools.js';
 import Download from './Download/Download.js';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
 
 function App() {
   return (
