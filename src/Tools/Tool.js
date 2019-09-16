12881b5774f65b6d3db58a5923313387f1468077
Екатерина Гавренко <kategavua@gmail.com>
2019-09-16 09:57:34 +0100

    Fix edit inline (propperty switches)

--- src/Tools/Tool.js
+++ src/Tools/Tool.js
@@ -6,29 +6,30 @@ export default class Tool extends Component {
         this.state = {
             visibleBlock: false,
             show: '',
-            colors: this.props.colors, 
+            bgcolors: this.props.bgcolors, 
             frames: this.props.frames,
             fonts: this.props.fonts,
-            active: this.props.title.toLowerCase()
+            active: this.props.title.toLowerCase(),
+            colors: this.props.colors,
+            openProp: this.props.openProp
         }
     }
 
     on_Click() {
         this.setState({
             visibleBlock: !this.state.visibleBlock,
-            show: 'active'
+            show: 'active',
+            openProp: !this.props.openProp
         })
     }
 
 
     render() {
-        // console.log(this.state[this.state.active], 'render')
-        
         return (
             <>
                 <div className="accordion__section">
                     <button className="accordion" onClick={() => this.on_Click()}>
-                        <p className="accordion__title">{this.props.title}</p>
+                        <p className="accordion__title">{this.props.name}</p>
                     </button>
                     {this.state[this.state.active].map(value => (
                         <div key={value} className={`showText ${this.state.visibleBlock ? "" : " hidden"}`}>
