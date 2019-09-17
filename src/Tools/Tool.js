8cac0a929437daa47dd35e2e957977da79b177e6
Екатерина Гавренко <kategavua@gmail.com>
2019-09-17 13:33:21 +0100

    Commit changes

--- src/Tools/Tool.js
+++ src/Tools/Tool.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 
 export default class Tool extends Component {
@@ -43,52 +42,4 @@ export default class Tool extends Component {
             </>
         );
     }
-=======
-import React, { Component } from 'react';
-
-export default class Tool extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            visibleBlock: false,
-            show: '',
-            frames: this.props.frames,
-            fonts: this.props.fonts,
-            active: this.props.title.toLowerCase(),
-            colors: this.props.colors,
-            open: true,
-            class: "accordion__section"
-        }
-    }
-
-    on_Click() {
-        this.setState({
-            open:  !this.state.open,
-            class: "accordion__section",
-            visibleBlock: !this.state.visibleBlock,
-            show: 'active'
-        })
-    }
-    render() {
-        console.log(this.state.open)
-        // console.log(this.state[this.state.active], 'render')
-        
-        return (
-            <>
-                <div className={this.state.class}>
-                    <button className="accordion" onClick={() => this.on_Click()}>
-                        <p className="accordion__title">{this.props.title}</p>
-                    </button>
-                    {this.state[this.state.active].map(value => (
-                        <div key={value}>
-                            <div className='accordion__text' value={value}
-                                onClick={() => this.props.changeProperty(this.state.active, value)}>{value}</div>
-                        </div>
-                        )
-                    )}
-                </div>
-            </>
-        );
-    } 
->>>>>>> 648b2cca48d4521d7d8d8a4bbc4c781d128ef056
 }
