edc0fa7f96410e61f8641aeef03076ebf333c7d5
Kateryna Havrenko <kategavua@gmail.com>
2019-09-17 12:54:38 +0100

    commit for pull request (#10)

--- src/Tools/Tool.js
+++ src/Tools/Tool.js
@@ -6,32 +6,35 @@ export default class Tool extends Component {
         this.state = {
             visibleBlock: false,
             show: '',
-            colors: this.props.colors, 
             frames: this.props.frames,
             fonts: this.props.fonts,
-            active: this.props.title.toLowerCase()
+            active: this.props.title.toLowerCase(),
+            colors: this.props.colors,
+            open: true,
+            class: "accordion__section"
         }
     }
 
     on_Click() {
         this.setState({
+            open:  !this.state.open,
+            class: "accordion__section",
             visibleBlock: !this.state.visibleBlock,
             show: 'active'
         })
     }
-
-
     render() {
+        console.log(this.state.open)
         // console.log(this.state[this.state.active], 'render')
         
         return (
             <>
-                <div className="accordion__section">
+                <div className={this.state.class}>
                     <button className="accordion" onClick={() => this.on_Click()}>
                         <p className="accordion__title">{this.props.title}</p>
                     </button>
                     {this.state[this.state.active].map(value => (
-                        <div key={value} className={`showText ${this.state.visibleBlock ? "" : " hidden"}`}>
+                        <div key={value}>
                             <div className='accordion__text' value={value}
                                 onClick={() => this.props.changeProperty(this.state.active, value)}>{value}</div>
                         </div>
@@ -40,5 +43,5 @@ export default class Tool extends Component {
                 </div>
             </>
         );
-    }
+    } 
 }
