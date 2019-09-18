3fde46c63e39fa8d9f3c50f3e02bcd202dbbc534
Sebastian <mastropieros@gmail.com>
2019-09-18 15:38:43 +0100

    add download button

--- src/Download/Download.js
+++ src/Download/Download.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import './Download.css';
 
 
+
+
 export default class Download extends Component {
     render() {
-      return <div className='download'>
-          <p>Download</p>
+      return <>
+      <div className='download'>
+          <button className='dwn-button'></button>
+          <div className='dwn-text'>Download</div>
       </div>
+      <div className='download'>
+        
+      </div>
+      </>
     }
 }
