7823cac9819e6645c08c1c3ec03f687d1bd70e65
Sebastian <mastropieros@gmail.com>
2019-09-23 10:07:50 +0100

    change home to save button

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
     render() {
       return <div className='navbar'>
         <label className='navlogo'>KS Card Creator</label>
-        <button className='button'>Home</button>
+        <button className='button'>Save</button>
         {/* <button className='button'>Reset</button> */}
       </div>
     }
